070ca7588ef676f6c9d38927a9bc85b418c7b5e4
edmond <edmond.cao@rd.group>
2024-06-15 15:27:14 +0800

    chore: 增加连接钱包按钮

---
+++ app/components/Wallet.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from '@nextui-org/react'
+
+const Wallet = () => {
+  return <Button>Wallet1</Button>
+}
+
+export default Wallet
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@nextui-org/react'
-import Image from 'next/image'
+import Wallet from './components/Wallet'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Wallet />
       <Button>按钮</Button>
     </main>
   )
